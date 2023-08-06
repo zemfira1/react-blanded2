0ba1a546dbc2211ff1611249360c72e825973d02
zemfira1 <craftyzema@gmail.com>
2023-08-06 08:27:05 -0400

    Zemfira

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,36 @@
-import { Component } from 'react';
+import { useState } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
-export class SearchForm extends Component {
-  render() {
-    return <h2>SearchForm</h2>;
-  }
-}
+export const SearchForm = ({ handleSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    if (!query) {
+      alert('Заполните форму, пожалуйста!');
+    }
+    handleSubmit(query);
+  };
+
+  return (
+    <SearchFormStyled onSubmit={onSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        onChange={handleChange}
+        value={query}
+      />
+    </SearchFormStyled>
+  );
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,14 +1,28 @@
-import { Component } from 'react';
-
-import * as ImageService from 'service/image-service';
+import { useEffect, useState } from 'react';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { getImages } from 'service/image-service';
 
-export class Gallery extends Component {
-  render() {
-    return (
-      <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
-      </>
-    );
-  }
-}
+export const Gallery = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+
+  const handleSubmit = value => {
+    console.log(value);
+    setQuery(value);
+  };
+
+  useEffect(() => {
+    if (!query) return;
+
+    getImages(query, page).then(data => {
+      console.log(data);
+    });
+  }, [query, page]);
+
+  return (
+    <>
+      <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+      <SearchForm handleSubmit={handleSubmit} />
+    </>
+  );
+};
